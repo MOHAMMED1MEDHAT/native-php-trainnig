ee09ab2729cc0c3f2f8edbae68d8b7b3680886d4
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-09-30 12:52:33 +0300

    Adding resistor color code challnge solution 1.0

--- Resistor Colors/index.php
+++ Resistor Colors/index.php
@@ -1,7 +1,30 @@
 <?php
+
+declare(strict_types=1);
+
 // 1. Create function for accepting the color as a string.
 // 2. Create an array resistor colors with their relative numeric code
 // 3. Return a numeric code based on the color's name
-function colorCode($color)
-{
+function colorCode(string $color):?int{
+  $resistorMap=['black'=>0,'brown'=>1,
+'red'=>2,
+'orange'=> 3,
+'yellow'=> 4,
+'green'=> 5,
+'blue'=>6,
+'violet'=> 7,
+'grey'=>8,
+'white'=> 9,
+];
+
+    foreach($resistorMap as $key=>$value){
+      if(strtolower($color)===$key){
+        return $value;
+      }
+    }
+
+  return null;
+    
 }
+
+var_dump(colorCode('black'));
