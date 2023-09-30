ba869bc56159e3b790da10ef24091e6f49c416d9
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-09-30 17:25:48 +0300

    Adding  Series challenge solution 1.0

---
+++ Series/index.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+function slices(string $series, int $size) : array{
+  $seriesLength = strlen($series);
+  
+  if ($size > $seriesLength || $size < 1) {
+    return [];
+  }
+  
+  $results = [];
+  
+  for ($i = 0; $i <= $seriesLength - $size; $i++) {
+    $results[] = substr($series, $i, $size);
+  }
+  
+ return $results; 
+}
+
+var_dump(slices('abcdef',4));
