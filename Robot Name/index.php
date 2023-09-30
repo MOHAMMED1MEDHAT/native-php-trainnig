d692fd71c243e784c925c44f1a12d8dfc7074cd5
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-09-30 16:42:47 +0300

    Adding robot names challenge solution 1.0

---
+++ Robot Name/index.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+function getNewName(): string{
+  
+  $letters=range('A','Z');
+  
+  $number=mt_rand(100,999);
+  
+  shuffle($letters);
+  
+  return "{$letters[0]}{$letters[1]}{$number}";  
+}
+
+echo getNewName();
